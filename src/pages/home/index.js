6e5de7a152be1f4ade4daf3e7990bb72e7c90e43
loadfms <loadfms@gmail.com>
2019-02-10 15:39:25 -0200

    fixes

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,24 +1,37 @@
 import React, { Component } from 'react'
+import SelectorInput from './../../components/selectorinput/index'
 
 export default class home extends Component {
     render() {
         return (
             <div className="main">
-                <div className="container">
-                    <div className="home-search">
-                        <div className="row">
-                            <h1 className="col-sm-12">Encontre o serviço que você procura na hora que precisa</h1>
-                            <p className="col-sm-12">Dezenas de salões perto de você: só falta encaixar o seu horário livre na agenda de um deles</p>
-                        </div>
-                        <div className="box">
+                <div className="container home-search">
+                    <div className="row">
+                        <h1 className="col-xs-12">Encontre o serviço que você procura na hora que precisa</h1>
+                        <p className="col-xs-12 subtitle">Dezenas de salões perto de você: só falta encaixar o seu horário livre na agenda de um deles</p>
+                    </div>
+                    <div className="row">
+                        <div className="col-xs-offset-1 col-xs-10 box ">
                             <h2>Salões</h2>
-                            <label>Data e hora</label>
-                            <input type="text"></input>
-                            
+
+                            <div class="row">
+                                <div class="col-xs-12">
+                                    <label>Data</label>
+                                    <SelectorInput icon="calendar" value="10/02/2019 15:00" />
+                                </div>
+
+                                <div class="col-xs-12">
+                                    <label>Serviço</label>
+                                    <SelectorInput icon="cut" value="Corte Feminino" />
+                                </div>
+                                <div class="col-xs-12">
+                                    <button className="btn"> Encontrar </button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </div >
         )
     }
 }
