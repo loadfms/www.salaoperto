a705c7bfd8849c5e1adfc16df581b9b07cf02031
Leonardo <loadfms@gmail.com>
2019-02-16 15:29:04 -0200

    chnge - logo

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -29,8 +29,8 @@ export default class navbar extends Component {
                             <button className="col-sm-12 icon hidden-md hidden-lg" onClick={this.togglemenu}>
                                 <i className="fa fa-bars"></i>
                             </button>
-                            <a href="/" className="brand col-md-4 hidden-sm hidden-xs">SP</a>
-                            <a href="#news" className="col-md-2 hidden-sm hidden-xs">Como funciona?</a>
+                            <a href="/" className="brand col-md-2 hidden-sm hidden-xs"></a>
+                            <a href="#news" className="col-md-offset-2 col-md-2 hidden-sm hidden-xs">Como funciona?</a>
                             <a href="#about" className="col-md-2 hidden-sm hidden-xs">Cadastrar meu salão</a>
                             <a href="/cadastro" className="col-md-2 hidden-sm hidden-xs">Cadastre-se</a>
                             <a href="#about" className="col-md-2 hidden-sm hidden-xs">Login</a>
