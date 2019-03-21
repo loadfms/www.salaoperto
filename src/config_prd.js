8b36ded62059a91f502e9d7ae3fdd643fb0df1bf
loadfms <loadfms@gmail.com>
2019-03-21 19:52:20 -0300

    add - env to build

---
+++ src/config_prd.js
@@ -0,0 +1,2 @@
+export const API_URL = "https://api.salaoperto.com.br/v1/"
+export const GOOGLE_GEOLOCATION_URL = "https://maps.googleapis.com/maps/api/geocode/json?latlng={lat},{lon}&sensor=true&key=$GOOGLEID"
