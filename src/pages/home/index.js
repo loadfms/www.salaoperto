848add45ccb91eb9434924e2d0cbef1b010bc5f2
loadfms <loadfms@gmail.com>
2019-03-23 19:47:02 -0300

    add - desktop home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -61,11 +61,11 @@ export default class home extends Component {
         this.props.history.push('/resultado')
     }
 
-    signin(){
+    signin() {
         this.props.history.push('/login')
     }
 
-    signup(){
+    signup() {
         this.props.history.push('/cadastro')
     }
 
@@ -75,29 +75,57 @@ export default class home extends Component {
 
     render() {
         return (
-            <div className="home-page container">
-                <div className="home-page__logo">
-                    <div className="row">
-                        <h1 className="col-xs-12 center">Salão Perto</h1>
+            <div className="home-page ">
+                <div className="home-page-mobile container hidden-md hidden-lg hidden-xl">
+                    <div className="home-page-mobile__logo">
+                        <div className="row">
+                            <h1 className="col-xs-12">Salão Perto</h1>
+                        </div>
+                    </div>
+                    <div className="home-page-mobile__buttons">
+                        <div className="row">
+                            <input type="submit" className="button col-xs-offset-1 col-xs-10" value="Entrar" onClick={this.signin} />
+                        </div>
+                        <div className="row">
+                            <input type="submit" className="button col-xs-offset-1 col-xs-10" value="Cadastrar" onClick={this.signup} />
+                        </div>
+                    </div>
+                    <div className="home-page-mobile__footer">
+                        <div className="row">
+                            <button className="col-xs-12">Como funciona?</button>
+                        </div>
+                        <div className="row">
+                            <button className="col-xs-12">Cadastre seu salão.</button>
+                        </div>
                     </div>
                 </div>
-                <div className="home-page__buttons">
-                    <div className="row">
-                        <input type="submit" className="button col-xs-offset-1 col-xs-10" value="Entrar" onClick={this.signin} />
+                <div className="home-page-desktop hidden-sm hidden-xs">
+                    <div className="home-page-desktop__content">
+                        <div className="home-page-desktop__content__logo">
+                            <div className="row">
+                                <h1 className="col-md-offset-1 col-md-11">salaoperto</h1>
+                                <h2 className="col-md-offset-1 col-md-7">Nunca foi tão fácil ter um novo visual</h2>
+                                <h3 className="col-md-offset-1 col-md-7">Encontre milhares de salões pertinho de você e marque seu horário. Receba confirmações e lembretes dos seus compromissos.</h3>
+                            </div>
+                        </div>
+                        <div className="home-page-desktop__content__buttons">
+                            <div className="row">
+                                <input type="submit" className="button col-md-offset-1 col-md-6" value="Entrar" onClick={this.signin} />
+                                <button type="submit" className="button gray col-md-offset-1 col-md-1" onClick={this.signup}><i class="icofont-search"></i></button>
+                            </div>
+                        </div>
+                        <div className="home-page-desktop__content__footer">
+                            <div className="row">
+                                <button className="col-xs-12">CADASTRE SEU SALÃO <span>Exiba seu salão para muitos clientes</span></button>
+                            </div>
+                        </div>
                     </div>
-                    <div className="row">
-                        <input type="submit" className="button col-xs-offset-1 col-xs-10" value="Cadastrar" onClick={this.signup}/>
+                    <div className="home-page-desktop__bg">
                     </div>
                 </div>
-                <div className="home-page__footer">
-                    <div className="row">
-                        <button className="col-xs-12">Como funciona?</button>
-                    </div>
-                    <div className="row">
-                        <button className="col-xs-12">Cadastre seu salão.</button>
-                    </div>
-                </div>
+
             </div>
+
         )
     }
 }
